4f0a4e99b6f7ad9e0ec2e8f9ee8bada911e68308
Masesora <direccionmasesora@gmail.com>
2026-01-14 11:32:40 +0100

    Remove Dockerfiles and DOCS folder

--- masesora_backend/DOCS/db.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-
-client = AsyncIOMotorClient("mongodb://localhost:27017")
-db = client["masesora"]
-
-def get_collection(name: str):
-    return db[name]
